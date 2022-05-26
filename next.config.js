f1116f78702f1bd84dc62d8488fecf4076979193
Md Tawsif Ul Hye Chowdhury <tawsif.lucse@gmail.com>
2022-05-26 15:15:02 +0600

    redirect from home page added to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/topic/module/1/topic-a',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
